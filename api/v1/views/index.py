d171663dd3001c7dc1ece1eed8729bea74c148cb
root <3290@holbertonschool.com>
2022-02-04 10:16:21 -0800

    api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,5 +8,5 @@ from flask import jsonify
 @app_views.route('/status')
 def status():
     """ Return Json Status """
-    json = {"Status" : "OK"}
-    return (jsonify(json))
+    json = {"status": "OK"}
+    return json
