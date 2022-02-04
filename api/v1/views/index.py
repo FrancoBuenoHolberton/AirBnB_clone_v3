0f6598de7131c2e15f324fc4148ae5b400260c26
root <3290@holbertonschool.com>
2022-02-04 15:22:37 -0800

    task-5medium

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,23 @@
 indexpy
 """
 from api.v1.views import app_views
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
 
 
 @app_views.route('/status')
 def status():
     """ Return Json Status """
     return {"status": "OK"}
+
+@app_views.route('/stats')
+def stats():
+    """ Stats """
+    stats['amenities'] = storage.count(Amenity)
+    stats['cities'] = storage.count(City)
+    stats['places'] = storage.count(Place)
+    stats['reviews'] = storage.count(reviews)
