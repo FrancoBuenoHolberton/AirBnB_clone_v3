847da17e1fe3c61d04ee6ad246d615d36a9f4b5c
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-02-04 20:41:07 -0300

    Prueba

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,11 +4,12 @@ indexpy
 """
 from api.v1.views import app_views
 from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.user import User
-from models.state import State
+#from models.city import City
+#from models.place import Place
+#from models.review import Review
+#from models.user import User
+#from models.state import State
+from models import storage
 
 
 @app_views.route('/status')
@@ -19,7 +20,5 @@ def status():
 @app_views.route('/stats')
 def stats():
     """ Stats """
-    stats['amenities'] = storage.count(Amenity)
-    stats['cities'] = storage.count(City)
-    stats['places'] = storage.count(Place)
-    stats['reviews'] = storage.count(reviews)
+    json = {"amenities": storage.count(Amenity)}
+    return json
