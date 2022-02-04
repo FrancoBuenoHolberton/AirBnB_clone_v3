eb8a7ca8d46bcdb509b9df22593414388ab95818
root <3290@holbertonschool.com>
2022-02-04 11:59:07 -0800

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -14,7 +14,7 @@ app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def downtear(self):
+def teardown_appcontext(self):
     """ Cierra storage"""
     storage.close()
 
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         host = '0.0.0.0'
 
     if getenv('HBNB_API_PORT'):
-        puerto = getenv('HBNB_API_PORT')
+        port = getenv('HBNB_API_PORT')
     else:
-        puerto = '5000'
-    app.run(host=host, port=puerto, threaded=True)
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,10 +3,8 @@
 indexpy
 """
 from api.v1.views import app_views
-from flask import jsonify
-
 
 @app_views.route('/status')
 def status():
     """ Return Json Status """
-    return {"status": "OK"}
+    return { "status": "OK" }
