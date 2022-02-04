1c5c8fe124c18de8a1c8cc127e250d6ec892acc8
root <3290@holbertonschool.com>
2022-02-04 10:20:43 -0800

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,7 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def downtear(self):
+    """ Cierra storage"""
     storage.close()
 
 
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,6 +5,7 @@ indexpy
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status')
 def status():
     """ Return Json Status """
