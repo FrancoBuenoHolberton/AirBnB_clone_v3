a2e3f9b79d64ae5b01c0a25ef4b22f39707ea86c
root <3290@holbertonschool.com>
2022-02-04 11:43:06 -0800

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,6 +6,6 @@ comment
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('__init__', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
